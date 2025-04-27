a2a5f9b55158e973acd3b46f3cd190739999e307
hasibprangon <hasibulhossainp09@gmail.com>
2025-04-27 13:07:12 +0600

    category section modified

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -11,10 +11,15 @@ import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import SectionTitle from '../../../Components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
         <div className='my-5'>
+            <SectionTitle 
+            subHeading={"From 11am to 12pm"}
+            heading={"Order Online"}
+            />
             <Swiper
                 slidesPerView={4}
                 spaceBetween={30}
@@ -23,7 +28,7 @@ const Category = () => {
                     clickable: true,
                 }}
                 autoplay={{
-                    delay: 2500,
+                    delay: 4700,
                     disableOnInteraction: false,
                   }}
                 navigation={true}
@@ -31,24 +36,24 @@ const Category = () => {
                 className="mySwiper"
             >
                 <SwiperSlide>
-                    <img src={slide1} alt="" />
-                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-16 text-white shadow-xl shadow-black'>Salads</h3>
+                    <img className='rounded-xl' src={slide1} alt="" />
+                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-20 text-white '>Salads</h3>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={slide2} alt="" />
-                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-16 text-white shadow-xl shadow-black'>Pizza</h3>
+                    <img className='rounded-xl' src={slide2} alt="" />
+                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-20 text-white '>Pizza</h3>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={slide3} alt="" />
-                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-16 text-white shadow-xl shadow-black'>Soup</h3>
+                    <img className='rounded-xl' src={slide3} alt="" />
+                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-20 text-white '>Soup</h3>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={slide4} alt="" />
-                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-16 text-white shadow-xl shadow-black'>Dessert</h3>
+                    <img className='rounded-xl' src={slide4} alt="" />
+                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-20 text-white '>Dessert</h3>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src={slide5} alt="" />
-                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-16 text-white shadow-xl shadow-black'>Salad</h3>
+                    <img className='rounded-xl' src={slide5} alt="" />
+                    <h3 className='md:text-4xl text-xl uppercase text-center -mt-20 text-white '>Salad</h3>
                 </SwiperSlide>
             </Swiper>
         </div>
